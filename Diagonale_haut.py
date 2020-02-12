fe5f1cdb4c892b3acb8a6bba5f1e547a96c60e2b
AllanDiny <allan.diny@eleve.egd.mg>
2020-02-12 09:18:05 +0100

    1

--- Diagonale_haut.py
+++ Diagonale_haut.py
@@ -1,13 +1,13 @@
 def diag_haut(gril, j, lig, col):
-    compteur=0
+    p=0
     if lig<4:
         return False
     if col>2:
         return False
     for i in range(4):
         if gril[lig-i][col+i]==j:
-            compteur+=1
-    if compteur==4:
+            p+=1
+    if p==4:
             return True
     return False
 
